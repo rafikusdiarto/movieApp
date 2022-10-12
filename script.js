0707e9d893e598e9014a2430dc6a09b30f38160f
rafikusdiarto <rafikusdiarto1707@gmail.com>
2022-10-12 14:19:54 +0700

    nambahSearch

--- script.js
+++ script.js
@@ -1,41 +1,55 @@
 const getDataApi = () =>{
     const URL = 'https://api.themoviedb.org/3/discover/movie?api_key=65cf174fae8bc3274e79960d1c5782b6&sort_by=popularity.desc';
+    const img_URL = 'https://image.tmdb.org/t/p/w500/';
     // const options = {
-    //     method: "GET"
-    // };
-
-    fetch(URL)
-    .then((response) => response.json())
-    .then((result) => {
-        let film = document.getElementById("kontenFilm");
-
-        let dataFilm = result.results;
-
-        dataFilm.forEach(index => {
-            film.innerHTML += `
-            <div class="col-4 mx-auto">
+        //     method: "GET"
+        // };
+        
+        fetch(URL)
+        .then((response) => response.json())
+        .then((result) => {
+            let film = document.getElementById("kontenFilm");
+            
+            let dataFilm = result.results;
+            
+            dataFilm.forEach(index => {
+                film.innerHTML += `
+                <div class="col-4 mx-auto">
                 <div class="card h-100">
-                    <img src="https://image.tmdb.org/t/p/w500/`+index.poster_path+`" class="card-img-top" alt="...">
-                    <div class="card-body">
-                        <div class="d-flex justify-content-between">
+                <img src="`+img_URL+``+index.poster_path+`" class="card-img-top" alt="...">
+                <div class="card-body">
+                <div class="d-flex justify-content-between">
                             <h5 class="card-title">`+index.title+`</h5>
                             <p class="card-text">Rating : <b>`+index.vote_average+`</b><p>
-                        </div>
-                    </div>
-                    <p class="card-text p-2">Realese Date : <b>`+index.release_date+`</b></p>
-                </div>
-            </div>
-            `
-            console.log(index)
-
-        });
-        // console.log(dataFilm)
-        // console.log(movieHTML)
-        // console.log(result)
-    })
-
-
-    .catch((error) => console.log(error));
-
+                            </div>
+                            </div>
+                            <p class="card-text p-2">Realese Date : <b>`+index.release_date+`</b></p>
+                            </div>
+                            </div>
+                            `
+                            // console.log(index)
+                            
+                        });
+                        // console.log(dataFilm)
+                        // console.log(movieHTML)
+                        // console.log(result)
+                    })
+                    .catch((error) => console.log(error));
+                    
 };
 getDataApi()
+
+
+function setName() {
+    const search_URL = 'https://api.themoviedb.org/3/search/movie?api_key=65cf174fae8bc3274e79960d1c5782b6&query=$';
+    let search = document.getElementById("search");
+    let setSearch = search.value;
+
+    if (setSearch && setSearch !== '') {
+        getDataApi(search_URL+setSearch)
+        setSearch='';
+    } else {
+        window.location.reload()
+    } 
+}
+
